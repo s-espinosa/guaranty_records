d3da19fd9fcb45a389d30ced03740df6a6ab779d
Sal Espinosa <sespinos@gmail.com>
2017-03-22 22:13:04 -0600

    Add callbackgs

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,10 @@
 class Invoice < ApplicationRecord
   belongs_to :employee
   belongs_to :album
+
+  before_save :set_total_price
+
+  def set_total_price
+    self.total_price = quantity * album.price
+  end
 end
