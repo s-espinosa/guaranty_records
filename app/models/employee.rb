fc16683d050644095085f541088f6ccce3d3f1f5
Sal Espinosa <sespinos@gmail.com>
2017-03-22 22:34:07 -0600

    Implement helper

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,11 +2,11 @@ class Employee < ApplicationRecord
   has_many :invoices
   has_many :albums, through: :invoices
 
-  def full_name
-    first_name + " " + last_name
-  end
-
   def total_revenue
     invoices.total_revenue
+  end
+
+  def formatted_total_revenue
+    "$#{sprintf('%.2f', total_revenue)}"
   end
 end
